67a281f380a6353cde445f533372851cb91e5f7b
Zuocheng Li <zuocheng@usc.edu>
2025-03-11 21:41:10 -0700

    Change Tank Upgrade

--- Assets/Scripts/TankTowerController.cs
+++ Assets/Scripts/TankTowerController.cs
@@ -51,7 +51,21 @@ public class TankTowerController : TowerController
         GameObject projectileObj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
         Projectile projectile = projectileObj.GetComponent<Projectile>();
         projectile.target = target;
-        projectile.damage = attackDamage;
+        projectile.damage = CalculateDamage();
+        //Debug.Log("Tower Rank: " + rankValue + " | Damage: " + projectile.damage);
         projectile.InitializeProjectile();
     }
+
+    float CalculateDamage()
+    {
+        switch (rankValue)  // 直接根据 rankValue 返回固定伤害
+        {
+            case 1: return 20f;  
+            case 2: return 40f;  
+            case 3: return 60f;  
+            case 4: return 80f;  
+            default: return 10f;  
+        }
+    }
+
 }
