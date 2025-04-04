d431372dd235eedd50b64fc65f42f36d49ca161e
hxrsherry <hexinrui@usc.edu>
2025-04-03 22:09:46 -0700

    CreateTowerDictUI on MainGame Page

---
+++ Assets/TowerGalleryController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TowerGalleryController : MonoBehaviour
+{
+   public GameObject galleryPanel;
+   public GameObject[] towerPanels; // 对应五个塔的介绍 UI
+
+   public void OpenGallery()
+   {
+      galleryPanel.SetActive(true);
+      Time.timeScale = 0f;
+   }
+
+   public void OpenTowerPanel(int index)
+   {
+      galleryPanel.SetActive(false);
+      towerPanels[index].SetActive(true);
+   }
+
+   public void ContinueGame(GameObject towerPanel)
+   {
+      towerPanel.SetActive(false);
+      Time.timeScale = 1f;
+   }
+}
